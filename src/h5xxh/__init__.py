161475fa640e04d2782ec99267d601e77f6a6179
Stefano Miccoli <stefano.miccoli@polimi.it>
2024-08-02 16:02:10 +0200

    Add actual code

--- src/h5xxh/__init__.py
+++ src/h5xxh/__init__.py
@@ -1,3 +1,29 @@
 # SPDX-FileCopyrightText: 2024-present S. Miccoli <stefano.miccoli@polimi.it>
 #
 # SPDX-License-Identifier: MIT
+import h5py
+import xxhash
+
+
+class Walker:
+    def __init__(self, /, hashfun=xxhash.xxh3_128):
+        self._names = []
+        self._digest = hashfun()
+
+    def __call__(self, name, obj):
+        if isinstance(obj, h5py.Dataset):
+            assert name > (
+                self._names[-1] if self._names else ""
+            ), f"Visiting in unexpected order: {name}, {self._names}"
+            self._names.append(name)
+            self._digest.update(obj[()])
+
+    @property
+    def hexdigest(self):
+        """Digest of the visited 'Dataset's"""
+        return self._digest.hexdigest()
+
+    @property
+    def names(self):
+        """Names of the visited 'Dataset's"""
+        return self._names
--- src/h5xxh/cli/__init__.py
+++ src/h5xxh/cli/__init__.py
@@ -1,12 +1,35 @@
 # SPDX-FileCopyrightText: 2024-present S. Miccoli <stefano.miccoli@polimi.it>
 #
 # SPDX-License-Identifier: MIT
-import click
+import sys
 
+import click
+import h5py
+
+from h5xxh import Walker
 from h5xxh.__about__ import __version__
 
 
-@click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
+@click.group(
+    context_settings={"help_option_names": ["-h", "--help"]},
+    invoke_without_command=True,
+)
 @click.version_option(version=__version__, prog_name="h5xxh")
-def h5xxh():
-    click.echo("Hello world!")
+@click.argument("h5", nargs=-1)
+def h5xxh(h5):
+    for pth in h5:
+        try:
+            msg = data_hash(pth)
+        except FileNotFoundError:
+            click.secho(f"{pth}: not found", file=sys.stderr, fg="red")
+        except OSError as err:
+            click.secho(f"{pth}: {err}", file=sys.stderr, fg="red")
+        else:
+            click.echo(f"{msg}  {pth}")
+
+
+def data_hash(pth):
+    callback = Walker()
+    with h5py.File(pth, "r") as h5:
+        h5.visititems(callback)
+    return callback.hexdigest
