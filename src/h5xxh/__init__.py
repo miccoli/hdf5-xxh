79fc7fbc67e55229535e7e39880ffa73af6fc2d7
Stefano Miccoli <stefano.miccoli@polimi.it>
2024-08-06 10:08:04 +0200

    Do not hash numpy.object_ arrays
    Memory in np.object_ arrays is not actual data, but pointers to python
    objects: do not hash.
    
    Add a logging.Logger with name equal to the module name, and
    log at warning level the name of 'h5py.Dataset's that are not hashed.

--- src/h5xxh/__init__.py
+++ src/h5xxh/__init__.py
@@ -1,8 +1,12 @@
 # SPDX-FileCopyrightText: 2024-present S. Miccoli <stefano.miccoli@polimi.it>
 #
 # SPDX-License-Identifier: MIT
+import logging
+
 import h5py
 import xxhash
+
+logger = logging.getLogger(__name__)
 
 
 class Walker:
@@ -11,12 +15,28 @@ class Walker:
         self._digest = hashfun()
 
     def __call__(self, name, obj):
-        if isinstance(obj, h5py.Dataset):
-            assert name > (
-                self._names[-1] if self._names else ""
-            ), f"Visiting in unexpected order: {name}, {self._names}"
-            self._names.append(name)
-            self._digest.update(obj[()])
+        # runtime check of lexicographical iteration order
+        assert name > (
+            self._names[-1] if self._names else ""
+        ), f"Visiting in unexpected order: {name}, {self._names}"
+        if not isinstance(obj, h5py.Dataset):
+            return
+        # Some object are skipped, and do not contribute to the overall hash.
+        # Currently skipped:
+        # - object arrays ('O') contain pointers to python Objects: it is
+        #   in general impossible to compute an immutable hash
+        if obj.dtype.hasobject:
+            logger.warning(
+                "%s: cannot hash '%s' (%s)",
+                obj.file.filename,
+                obj.name,
+                obj.dtype.str,
+            )
+            return
+
+        logger.debug("hashing '%s' (%s)", obj.name, obj.dtype.str)
+        self._names.append(name)
+        self._digest.update(obj[()])
 
     @property
     def hexdigest(self):
